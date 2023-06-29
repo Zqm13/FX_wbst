f8d5d95e282185684d10cb2047f9f1fe68367bff
Zqm13 <132812324+Zqm13@users.noreply.github.com>
2023-06-29 17:02:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ db.init_app(app)
 
 
 with app.app_context():
-    #db.create_all()
-    #populate_table_with_json_data('table_values.json')
+    db.create_all()
+    populate_table_with_json_data('table_values.json')
     write_table_to_json_file('output.json')
     VALUES_FILE_PATH = 'output.json'
 
